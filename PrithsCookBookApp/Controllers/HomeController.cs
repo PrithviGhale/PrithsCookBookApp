faaa59caee29ad7130f0a0b2bd4eae9be6d4269a
Prithvi Ghale <prithvighale01@gmail.com>
2025-01-16 21:44:52 -0500

    Initial commit for PrithsCookBookApp

---
+++ PrithsCookBookApp/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using PrithsCookBookApp.Models;
+
+namespace PrithsCookBookApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View("HomePage");
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ PrithsCookBookApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PrithsCookBookApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
